ab4f392f53ab660e44fb01ece88a626573ca5229
Andressa <146253485+AndresssaSMachado@users.noreply.github.com>
2024-04-19 14:07:26 -0300

    Add files via upload

---
+++ Alura midi/main.js
@@ -0,0 +1,47 @@
+function tocaSom(selectorAudio) {
+    const elemento = document.querySelector(selectorAudio);
+
+    if (elemento != null && elemento.localName ==='audio') {
+            elemento.play();
+           }
+    else {
+        // alert "Elemento não encontrado"
+        alert('Elemento não encontrado ou Seletor inválido')
+
+           }
+         }
+    
+  
+
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+// para
+for (let contador = 0; contador < listaDeTeclas.length; contador++) {
+    const tecla = listaDeTeclas[contador];
+    const instrumento = tecla.classList[1];
+    
+// template string
+    const idAudio = `#som_${instrumento}`;
+
+    tecla.onclick = function () {
+        tocaSom(idAudio);
+    }
+
+    // se
+    tecla.onkeydown = function (evento) {
+
+        if (evento.code === 'Space' || evento.code === 'Enter') {
+            tecla.classList.add('ativa');
+        }
+    }
+    
+
+    
+tecla.onkeyup = function () {
+    tecla.classList.remove('ativa');
+}
+
+
+
+
+}
